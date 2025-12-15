3d19bcea96da3aad31bef660c5ea4a0670a78b21
PCyganiuk <pcyganiuk@gmail.com>
2025-12-15 14:41:50 +0100

    Revert "torch architecture"
    This reverts commit b20a58ff346f96f23bc1a9ab918df80a5bf401fa.

--- data_manager/dataloaders.py
+++
@@ -1,8 +0,0 @@
-import torch 
-import torch.optim as optim
-import torch.nn as nn
-from torchvision.ops import sigmoid_focal_loss
-from torchvision.models import resnet50, mobilenet_v2, densenet121, ResNet50_Weights, MobileNet_V2_Weights, DenseNet121_Weights
-from config.config import ModelConfig
-
-
